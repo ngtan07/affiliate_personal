f9a3b55d6847d7f94a6730d5f4ebc4287727ded4
ngtan07 <ngthanhan0712@gmail.com>
2026-04-03 23:37:48 +0700

    inital project

---
+++ backend/src/test/java/com/example/affiliate/personal/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.affiliate.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
